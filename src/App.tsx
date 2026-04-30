054399625d095cfd533df7aebd449fd9f57719e7
Bosco Nie <Niebosco@gmail.com>
2026-04-30 11:40:59 -0400

    updated url

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { MainLayout } from '@/Mainlayout';
+import { MainLayout } from '@/MainLayout';
 import { CreditsView, ErrorView, HomeView, MovieView, NowPlayingView, ReviewsView, SearchView, TrendingView } from '@/views';
 import { Route, Routes } from 'react-router-dom';
 
@@ -7,12 +7,35 @@ export const App = () => {
         <Routes>
             <Route path="/" element={<HomeView />} />
             <Route element={<MainLayout />}>
-                <Route path="/now-playing" element={<NowPlayingView />} />
-                <Route path="/trending" element={<TrendingView />} />
-                <Route path="/search" element={<SearchView />} />
-                <Route path="/movie/:id" element={<MovieView />}>
-                    <Route path="credits" element={<CreditsView />} />
-                    <Route path="reviews" element={<ReviewsView />} />
+                <Route path="/movies" element={<MovieView />}>
+                    <Route path="category/now_playing" element={<NowPlayingView />} />
+                    <Route path="category/popular" element={<ReviewsView />} />
+                    <Route path="category/top_rated" element={<ReviewsView />} />
+                    <Route path="category/upcoming" element={<ReviewsView />} />
+                </Route>
+                <Route path="/tv" element={<MovieView />}>
+                    <Route path="category/airing_today" element={<CreditsView />} />
+                    <Route path="category/on_the_air" element={<ReviewsView />} />
+                    <Route path="category/popular" element={<ReviewsView />} />
+                    <Route path="category/top_rated" element={<ReviewsView />} />
+                </Route>
+                <Route path="/trending" element={<MovieView />}>
+                    <Route path="movies" element={<CreditsView />} />
+                    <Route path="tv" element={<ReviewsView />} />
+                </Route>
+                <Route path="/genre" element={<MovieView />}>
+                    <Route path="movies" element={<CreditsView />} >
+                        <Route path="action" element={<CreditsView />} />
+                        <Route path="adventure" element={<ReviewsView />} />
+                        <Route path="animation" element={<ReviewsView />} />
+                        <Route path="family" element={<ReviewsView />} />
+                    </Route>
+                    <Route path="tv" element={<ReviewsView />} >
+                        <Route path="action" element={<CreditsView />} />
+                        <Route path="animation" element={<ReviewsView />} />
+                        <Route path="comedy" element={<ReviewsView />} />
+                        <Route path="family" element={<ReviewsView />} />
+                    </Route>
                 </Route>
             </Route>
             <Route path="*" element={<ErrorView />} />
